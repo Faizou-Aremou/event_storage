b38f30ebf74a99f9d3852df477c939c15f21d8dc
Faizou <faizouaremou@gmail.com>
2022-11-26 13:41:21 +0100

    publish queue

--- src/sql/event-storage.sql
+++ src/sql/event-storage.sql
@@ -2,8 +2,10 @@ create table Events (
     AggregateId CHAR(16),
     Data BLOB,
     Version INT(10),
+    SequenceNumber BIGINT,
     constraint pk_Events_Version_AggregateId primary key (Version, AggregateId),
-    constraint fk_Events_AggregateId foreign key (AggregateId) references Aggregates (AggregateId)
+    constraint fk_Events_AggregateId foreign key (AggregateId) references Aggregates (AggregateId),
+    constraint Unique_Events_SequenceNumber unique (SequenceNumber)
 );
 
 create table Aggregates (
